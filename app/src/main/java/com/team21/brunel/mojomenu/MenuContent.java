7b1027e3b78c7ec6d550c746579f4b7da22e42be
Ashwin Khatwa <ashwinkhatwa@hotmail.com>
2017-02-01 17:35:26 +0000

    Created Menu Content

---
+++ app/src/main/java/com/team21/brunel/mojomenu/MenuContent.java
@@ -0,0 +1,10 @@
+package com.team21.brunel.mojomenu;
+
+/**
+ * Created by Ashwin Khatwa on 01/02/2017.
+ */
+
+public class MenuContent {
+    String screenName;
+    String screenDesc;
+}
--- app/src/main/java/com/team21/brunel/mojomenu/MenuPage.java
+++ app/src/main/java/com/team21/brunel/mojomenu/MenuPage.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MenuPage extends AppCompatActivity {
 
@@ -16,7 +17,21 @@ public class MenuPage extends AppCompatActivity {
     // List Content used to populate the list
     private List<MenuContent> createList(int size) {
         List<MenuContent> result = new ArrayList<>();
+        List<String> titles = new ArrayList<>();
+        titles.add(getString(R.string.card_title_1));
+        // Add more titles if and when needed
 
+        List<String> desc = new ArrayList<>();
+        desc.add(getString(R.string.card_description_1));
+
+        for (int i = 0; i < size; i++) {
+            MenuContent mc = new MenuContent();
+            mc.screenName = titles.get(i);
+            mc.screenDesc = desc.get(i);
+
+            result.add(mc);
+        }
+        return result;
     }
 
 }
