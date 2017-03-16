07530f5a6f593053cefeff71941c7ad22dd1c7b5
Muhunthan1 <1514993@my.brunel.ac.uk>
2017-03-16 09:22:17 +0000

    database connection for starbucks

---
+++ starbucks.php
@@ -0,0 +1,24 @@
+<?php
+$host = "localhost";
+$db_user ="group21";
+$db_password="panos";
+$db_name = "test";
+
+$con = mysqli_connect($host,$db_user,$db_password,$db_name);
+$sql = "select Menuname from starbucks;";
+//$sql = "select * from subs;";
+//echo"connection made";
+$result =  mysqli_query($con,$sql);
+
+
+$response = array();
+
+while($row = mysqli_fetch_assoc($result))
+{
+array_push($response,array("Menuname"=>$row["Menuname"]));
+//array_push($response,array("Menuname"=>$row["Menuname"],"Price"=>$row["Price"]));
+	
+}
+echo json_encode($response);
+mysqli_close($con);
+?>
